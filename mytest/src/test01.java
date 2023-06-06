a9510f35320363ba8d140386447e8d37fa5861e9
NingKobe <48660994+NingKobe@users.noreply.github.com>
2023-06-06 09:38:22 +0800

    Update test01.java

--- mytest/src/test01.java
+++ mytest/src/test01.java
@@ -1,13 +1,6 @@
 public class test01 {
     public static void main(String[] args) {
-        String a = "12345678";
-        String b = "1234";
-        String c = b + "5678";
-        if (c == a) {
-            System.out.println(true);
-        }
-        else {
-            System.out.println(false);
-        }
+        
+            System.out.println("yes");
     }
 }
