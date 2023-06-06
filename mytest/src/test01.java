2a52c0e0424fcee23e8627a5cc76506e108e553f
NingKobe <48660994+NingKobe@users.noreply.github.com>
2023-06-06 09:37:51 +0800

    Add files via upload

---
+++ mytest/src/test01.java
@@ -0,0 +1,13 @@
+public class test01 {
+    public static void main(String[] args) {
+        String a = "12345678";
+        String b = "1234";
+        String c = b + "5678";
+        if (c == a) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+        }
+    }
+}
